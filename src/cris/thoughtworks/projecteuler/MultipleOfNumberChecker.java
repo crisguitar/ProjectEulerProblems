db8e1a76d05338ac538a22263c62396fb53870d3
Cris Pinto <cdpinto@me.com>
2014-04-22 10:14:50 -0500

    Added method to check if number is multiple of 5

--- src/cris/thoughtworks/projecteuler/MultipleOfNumberChecker.java
+++ src/cris/thoughtworks/projecteuler/MultipleOfNumberChecker.java
@@ -7,4 +7,8 @@ public class MultipleOfNumberChecker {
     public boolean isMultipleOf3(int number) {
         return (number % 3) == 0;
     }
+
+    public boolean isMultipleOf5(int number) {
+        return true;
+    }
 }
--- test/cris/thoughtworks/projecteuler/MultipleOfNumberCheckerTest.java
+++ test/cris/thoughtworks/projecteuler/MultipleOfNumberCheckerTest.java
@@ -8,10 +8,10 @@ import static junit.framework.Assert.assertTrue;
 public class MultipleOfNumberCheckerTest {
 
     @Test
-    public void shouldReturnTrueWhenCheckingIfNumber3IsMultipleOf3() {
+    public void shouldReturnTrueWhenCheckingIfNumberIsMultipleOf3() {
         MultipleOfNumberChecker multipleOfNumberChecker = new MultipleOfNumberChecker();
 
-        boolean isMultipleOf3 = multipleOfNumberChecker.isMultipleOf3(3);
+        boolean isMultipleOf3 = multipleOfNumberChecker.isMultipleOf3(6);
 
         assertTrue(isMultipleOf3);
     }
@@ -23,5 +23,15 @@ public class MultipleOfNumberCheckerTest {
         boolean isMultipleOf3 = multipleOfNumberChecker.isMultipleOf3(4);
 
         assertFalse(isMultipleOf3);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCheckingIfNumberIsMultipleOf5() {
+        MultipleOfNumberChecker multipleOfNumberChecker = new MultipleOfNumberChecker();
+
+        boolean isMultipleOf5 = multipleOfNumberChecker.isMultipleOf5(15);
+
+        assertTrue(isMultipleOf5);
+
     }
 }
